cfdd055fa08dfae562162c0f0d0f886457c6ee96
vitor quintanilha barbosa <vitorqb@gmail.com>
2018-10-28 14:19:08 +0100

    Added tests

--- test/mylisputils-test.el
+++ test/mylisputils-test.el
@@ -1,3 +1,32 @@
 ;;; mylisputils-test.el --- Tests for mylisputils
+(require 'cl-lib)
+
+(ert-deftest add-to-generic-path-works ()
+  (let ((stub-env "A"))
+    (cl-letf (((symbol-function 'getenv) (lambda (x) stub-env))
+              ((symbol-function 'setenv) (lambda (y x) (setq stub-env x))))
+      (myutils/add-to-generic-path "B" "")
+      (should (equal stub-env "B:A")))))
+
+(ert-deftest test-add-to-python-path ()
+  (let ((python-path "PATH1"))
+    (cl-letf (((symbol-function 'getenv) (lambda (&rest args) python-path))
+              ((symbol-function 'setenv) (lambda (y x) (setq python-path x))))
+      (myutils/add-to-pythonpath "PATH2")
+      (should (equal python-path "PATH2:PATH1")))))
+
+(ert-deftest test-call-shell-command-kill-buffer ()
+  (cl-letf (((symbol-function 'read-string) (-const "")))
+    (myutils/call-shell-command "echo 'hola'" "mybuff")
+    (should (equal (get-buffer "mybuff") nil))))
+
+(ert-deftest test-call-shell-command-does-not-kill-buffer ()
+  (cl-letf (((symbol-function 'read-string) (-const "keep")))
+    (myutils/call-shell-command "echo 'hola'" "buffname")
+    (should (not (equal (get-buffer "buffname") nil)))))
+
+(ert-deftest test-concat-file ()
+  (should (equal (myutils/concat-file "/home/vitor" "file") "/home/vitor/file"))
+  (should (equal (myutils/concat-file "/home/vitor/" "f") "/home/vitor/f")))
 
 ;;; mylisputils-test.el ends here
--- test/test-helper.el
+++ test/test-helper.el
@@ -1,3 +1,6 @@
 ;;; test-helper.el --- Helpers for mylisputils-test.el
-
+(require 'el-mock)
+(require 'dash)
+(require 'dash-functional)
+(require 'mylisputils)
 ;;; test-helper.el ends here
